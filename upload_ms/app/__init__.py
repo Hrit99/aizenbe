ec576db1ec10b1501b94af4e049800347e25a097
hritikp <hritik.pihal@oneture.com>
2024-10-11 08:07:57 +0530

    env set for koyeb

--- upload_ms/app/__init__.py
+++ upload_ms/app/__init__.py
@@ -7,13 +7,13 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')  
-app.config['AWS_ACCESS_KEY_ID'] = os.getenv('AWS_ACCESS_KEY_ID')
-app.config['AWS_SECRET_ACCESS_KEY'] = os.getenv('AWS_SECRET_ACCESS_KEY')
-app.config['AWS_S3_BUCKET_NAME'] = os.getenv('AWS_S3_BUCKET_NAME')
-app.config['AWS_REGION'] = os.getenv('AWS_REGION')
+app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY')  
+app.config['AWS_ACCESS_KEY_ID'] = os.environ.get('AWS_ACCESS_KEY_ID')
+app.config['AWS_SECRET_ACCESS_KEY'] = os.environ.get('AWS_SECRET_ACCESS_KEY')
+app.config['AWS_S3_BUCKET_NAME'] = os.environ.get('AWS_S3_BUCKET_NAME')
+app.config['AWS_REGION'] = os.environ.get('AWS_REGION')
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
